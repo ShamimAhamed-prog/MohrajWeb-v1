8a805221fc68c8a0fd4aef09524b3a38eb66d827
unknown <shamim.aiub2@gmail.com>
2021-12-02 20:05:33 +0600

    portfolio1

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -5,32 +5,55 @@
             <v-subheader class="text-h4 justify-center">Portfolio</v-subheader>
             <p>Hello This is company Portfolio</p>
         </v-col>
-        <v-col lg="3">
+        <v-col lg="12">
             <div class="text-center">
 
-                <v-chip class="ma-2" color="primary">
+                <v-chip  @click= "activePortfolio = 'digitalmarketing'"  class="mx-5" :color="activePortfolio == 'digitalmarketing'?'success':'' " >
                     Digital Marketing
                 </v-chip>
 
-                <v-chip class="ma-2" color="red" text-color="white">
+                <v-chip  @click= "activePortfolio= 'reseller'"  class="mx-5" :color="activePortfolio == 'reseller'?'success':'' " >
                     Reseller
                 </v-chip>
 
-                <v-chip class="ma-2" color="green" text-color="white">
+                <v-chip  @click= "activePortfolio= 'ecommerce'" class="mx-5" :color="activePortfolio == 'ecommerce'?'success':'' " >
                     E-commerce
                 </v-chip>
-                 <v-chip class="ma-2" color="green" text-color="white">
-                    Nothing
-                </v-chip>
+               
             </div>
         </v-col>
+              <slot v-if="activePortfolio=='digitalmarketing'">
+                    <v-col lg="3" v-for="(web,webindex) in webApps" :key="webindex" >
+                    <v-card>
+                        <v-img 
+                        :src="web.image" 
+                        class="white--text align-end"
+                        gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)" 
+                        height="200px"
+                        >
+                            <v-card-title>{{web.title}}</v-card-title>
+                        </v-img>
+                    </v-card>
+                </v-col>
+              </slot>
     </v-row>
 </v-container>
 </template>
 
 <script>
 export default {
-    name: "Portfolio"
+    name: "Portfolio",
+     data(){
+        return{
+            activePortfolio:'digitalmarketing',
+            webApps:[
+                {title:'Digital Marketing', image: './../assets/digital.jpg'},
+                {title:'Reseller', image:'./../assets/reseller.png'},
+                {title:'E-commerce', image:'./../assets/ecommerce.png'}
+            ]
+
+        }
+    }
 }
 </script>
 
